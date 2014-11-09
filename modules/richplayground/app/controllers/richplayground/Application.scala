19e50618f3d5120f74bf4b245ecd08667bda5524
ialekseev <alekseevi15@gmail.com>
2014-11-09 18:38:43 +0300

    Uncomment forgotten line

--- modules/richplayground/app/controllers/richplayground/Application.scala
+++ modules/richplayground/app/controllers/richplayground/Application.scala
@@ -5,8 +5,7 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok("sdsd")
-    //Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.richplayground.index("Your new application is ready."))
   }
 
 }
